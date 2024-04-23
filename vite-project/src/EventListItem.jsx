b3390143ddba11a2f0a08c2fbc5db1f2fefbf88c
ajaybaghel05 <ajaybaghel1541@gmail.com>
2024-04-24 02:21:17 +0530

    first commit

--- vite-project/src/EventListItem.jsx
+++ vite-project/src/EventListItem.jsx
@@ -1,48 +1,73 @@
-import React from 'react';
-//import moment from 'moment';
-import {Link} from 'react-router-dom';
-import { faCalendarAlt, faClock, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  faCalendarAlt,
+  faClock,
+  faArrowRight,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-//import { history } from '../routers/AppRouter';
 
-export const EventListItem = ({ eventName="App dev", description="make an app", userImageUrl, time, userHandle="ansh", imageUrl, startTime="10:00", endTime="11:00",eventId=1, location, startDate="1-11-11", endDate="1-11-11", fee, id=1, venues }) => (
-    <div className={"list-card"+ " my-blogs__list-card" }>
-        <div className="list-card__body">
+export const EventListItem = (props) => {
+  const [isRegistered, setIsRegistered] = useState(false);
+
+  const toggleRegistration = () => {
+    setIsRegistered(!isRegistered);
+  };
+
+  return (
+    <>
+      <div className="event-list-item-container">
+        <div className={"list-card" + " my-blogs__list-card"}>
+          <div className="list-card__body">
             <div className="list-card__image">
-                {imageUrl ? <img src={imageUrl} alt="" /> : <img src='/images/img5.jpeg' alt="" />}
+              {(
+                <img src={props?.image} alt="" />
+              )}
             </div>
             <div className="list-card__content">
-               
-                <Link className="list-card__body-title" to={`/event/${id||eventId}`}>
-                    {eventName}
-                    <div className="list-card__body-author">
-                        <img src={userImageUrl} alt="" />
-                        <h3>{userHandle}</h3>
-                    </div>
-                    <div>
-                        <p className="list-card__content-text">{description.substring(0, 300) + ' ...'}</p>
-                    </div>
-                </Link>
+              <Link
+                className="list-card__body-title"
+                to={`/event/${props?.eventId}`}
+              >
+                {props?.eventName}
+                <div>
+                  <p className="list-card__content-text">{props?.description}</p>
+                </div>
+              </Link>
             </div>
             <div className="list-card__body-date">
-                <div><h6><FontAwesomeIcon icon={faClock}/>{" " + startTime + " - " + endTime}</h6></div>
-                <div>
-                    <h6><FontAwesomeIcon icon={faCalendarAlt} />{" " + startDate.split(',')[0] + (endDate !== startDate ? " - " + endDate.split(',')[0] : "")}</h6>
-                </div>
-                <ul>
-                    {/* <li><FontAwesomeIcon icon={faHeart}  size="lg"/></li>
-                    <li><FontAwesomeIcon icon={faComment}  size="lg"/></li>
-                    <li><FontAwesomeIcon icon={faShare} size="lg"/></li> */}
-                </ul>
+              <div>
+                <h6>
+                  <FontAwesomeIcon icon={faClock} />
+                  {" " + props?.startTime + " - " + props?.endTime}
+                </h6>
+              </div>
+              <div>
+                <h6>
+                  <FontAwesomeIcon icon={faCalendarAlt} />
+                  {" " +
+                    (props?.startDate?.split(",")[0] || '') +
+                    (props?.endDate !== props?.startDate ? " - " + (props?.endDate?.split(",")[0] || '') : "")}
+                </h6>
+              </div>
+              <div className="ven">
+                <h6>Venue:{props?.venue}</h6>
+              </div>
             </div>
-        </div>
+          </div>
 
-        <div className="list-card__fab">
-                <Link to={`/event/${id}`}>
-                    <FontAwesomeIcon icon={faArrowRight} size="2x" color="#eb6e80"/>
-                </Link>
-            </div>  
-    </div>
-);
+          <div className="list-card__fab">
+            <button
+              className={isRegistered ? "butunreg" : "butreg"}
+              onClick={toggleRegistration}
+            >
+              {isRegistered ? "Unregister" : "Register"}
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default EventListItem;
