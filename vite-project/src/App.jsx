61bff03707855d67e29ae73bbf72cb01b67d9d6e
ut_karsh_2 <utkar721@gmail.com>
2024-04-25 02:40:11 +0530

    Configured Tailwind and added Footer

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,15 +5,18 @@ import LoginPage from './LoginPage';
 import SignupPage from './SignupPage';
 import DashboardPage from './DashboardPage';
 import EventForm from './EventForm';
-
+import Layout from './Layout'
 const App = () => {
   return (
     <Router>
       <Switch>
+      
         <Route path="/signup" component={SignupPage} />
         <Route path="/dashboard" component={DashboardPage} />
         <Route path="/add" component={EventForm} />
         <Route path="/" component={LoginPage} />
+      
+
         
       </Switch>
     </Router>
